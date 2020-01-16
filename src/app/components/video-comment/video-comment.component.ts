269caed28402de9e22ae8da278076f75cde73d26
Oussama <ahmedoussama.amor@etu.usthb.dz>
2020-01-16 23:31:47 +0100

    adjust display data

--- src/app/components/video-comment/video-comment.component.ts
+++ src/app/components/video-comment/video-comment.component.ts
@@ -1,4 +1,5 @@
-import { Component, OnInit } from '@angular/core';
+import {Component, Input, OnInit} from '@angular/core';
+import Comment from '../../model/comment';
 
 @Component({
   selector: 'app-video-comment',
@@ -6,6 +7,8 @@ import { Component, OnInit } from '@angular/core';
   styleUrls: ['./video-comment.component.scss']
 })
 export class VideoCommentComponent implements OnInit {
+
+  @Input() comment: Comment;
 
   constructor() { }
 
